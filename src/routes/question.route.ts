c5e87adfac7c1bb03c081cc0b516dfa9c87f1609
meet-tola <wasiusikiru7@gmail.com>
2025-03-06 10:00:38 +0100

    remove question controller

--- src/routes/question.route.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getQuestions } from "../controllers/question.controller";
-
-const router = express.Router();
-router.get("/questions", getQuestions);
-
-export default router;
