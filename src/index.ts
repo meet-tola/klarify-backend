b84c12993aad0a29e5451b7a32d3a4cf00ae5ef4
meet-tola <wasiusikiru7@gmail.com>
2025-03-11 00:47:54 +0100

    add back middleware isAuth

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ app.get(
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
-app.use(`${BASE_PATH}/user`, userRoutes);
+app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/skills`, isAuthenticated, skillsRoutes);
 app.use(`${BASE_PATH}/career`, isAuthenticated, careerRoutes);
 app.use(`${BASE_PATH}/roadmap`, isAuthenticated, roadmapRoutes);
