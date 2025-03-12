e23214effa1c60e6b475e741ff6b39fb0f8f73ef
meet-tola <wasiusikiru7@gmail.com>
2025-03-12 21:55:10 +0100

    token for 24hours

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -4,13 +4,13 @@ import { config } from "../config/app.config";
 
 
 export const setToken = (res: Response, userId: string): string => {
-  const token = jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: "1h" });
+  const token = jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: "24h" });
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: config.NODE_ENV === "production",
     sameSite: config.NODE_ENV === "production" ? "none" : "lax",
-    maxAge: 60 * 60 * 1000,
+    maxAge: 24 * 60 * 60 * 1000, 
   });
 
   return token;
